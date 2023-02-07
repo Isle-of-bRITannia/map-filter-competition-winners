def4630b6bfa38cfe7fe014ce62c6661e6d867f6
chigbe001 <chigbe001@gmail.com>
2023-02-07 11:43:42 -0500

    Message

--- src/index.js
+++ src/index.js
@@ -7,3 +7,38 @@ import botwMonsters from './botw_monsters_cleaned.js';
 const equipment = botwEquipment;
 const monsters = botwMonsters;
 const materials = botwData.data.materials;
+
+let theGoodStuff1 = [];let theGoodStuff4 = [];botwMonsters.forEach(theGoodStuff2)
+{
+let theGoodStuff3 = [];
+theGoodStuff3 +=  theGoodStuff2;
+theGoodStuff1 = []
+}
+botwMonsters.forEach(theotherstuff)
+{
+theotherstuff.common_locations.forEach(qwjdhn)
+{
+if(qwjdhn == "Hyrule Field")
+{
+theGoodStuff1 += theotherstuff;
+}
+else{ theGoodStuff4 += theotherstuff}
+}
+}
+theGoodStuff1.forEach(object)
+{
+object.drops.forEach(otherObject)
+{
+if(otherObject)
+{
+theGoodStuff4 = [];
+}
+materials.forEach(thirdobject)
+{
+if(thirdobject.name == otherObject)
+{
+otherObject = thirdobject;
+}
+}
+}
+}
